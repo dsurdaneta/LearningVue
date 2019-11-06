2bf77a897110e448762e34f620b35ad0fcae5f59
Urdaneta Daniel (ERNI) <daniel.udaneta@erni-espana.es>
2019-11-06 13:39:21 +0100

    added aspnetcore-Vue-starter-master and created new component Hello.vue

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LearningVue
 just a tutorial following project
+
+Using NetCore Vue Starter: https://github.com/TrilonIO/aspnetcore-Vue-starter/blob/master/aspnetcore-vuejs.nuspec
---
+++ content/ClientApp/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <h1 class="mt-4">
+        {{msg}}
+      </h1>
+      <h2>Student Info</h2>
+      <p>Student Name: {{student.name}}</p>
+      <p>Student Age: {{student.age}}
+      <button @click="UpdateName()">Update Name</button></p>
+      {{3 + 5}}
+      {{3 > 5}}
+      {{student}}
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Ola k ase',
+      student: {
+        name: 'Daniel',
+        age: 33
+      }
+    }
+  },
+    methods: {
+      UpdateName: function () {
+        this.student.name = "new Name";
+      }
+    }
+
+}
+</script>
+
+<style>
+  h1{
+    color:red;
+  }
+</style>
