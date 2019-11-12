12f5985a72487080c5777446fe71fda377cc5be8
Daniel Urdaneta <daniel.urdaneta@contractors.roche.com>
2019-11-12 11:51:37 +0100

    Moved ListRendering component to the new Advanced folder, added Computed component and updated routes.js

---
+++ content/ClientApp/components/Advanced/Computed.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h3>Computed Properties are a great way to cache data</h3>
+    <p>The value is: {{val}}</p>
+    <button @click="CallComputed()">Get value</button>
+    <br/>
+    <h3>Computed properties with get/set</h3>
+    <p>{{active}}</p>
+    <button @click="updateComputed()">Update Value</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Computed properties',
+        val: '',
+        temp: ''
+      }
+    },
+    methods: {
+      CallComputed: function () {
+        this.val = this.computedData;
+      },
+      valueReturn: function () {
+        console.log("computed!");
+        return "amr";
+      },
+      updateComputed: function () {
+        this.active = "aaaa";
+      }
+    },
+    computed:
+    {
+      computedData: function () {
+        return this.valueReturn();
+      },
+      active: {
+        get: function () {
+          return this.temp;
+        },
+        set: function (value) {
+          this.temp = value;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- content/ClientApp/router/routes.js
+++ content/ClientApp/router/routes.js
@@ -8,7 +8,8 @@ import DataBinding from 'components/DataBinding'
 import Models from 'components/Models'
 import Routes from 'components/Routes'
 import Child from 'components/Child'
-import ListRendering from 'components/ListRendering'
+import ListRendering from 'components/Advanced/ListRendering'
+import Computed from 'components/Advanced/Computed'
 
 export const routes = [
   { name: 'home', path: '/', component: HomePage, display: 'Home', icon: 'home' },
@@ -17,17 +18,18 @@ export const routes = [
   { name: 'fetch-data', path: '/fetch-data', component: FetchData, display: 'Data', icon: 'list' },
   { name: 'Hello', path: '/Hello', component: Hello, display: 'Hello Vue', icon: 'info' },
   { name: 'Directives', path: '/Directives', component: Directives, display: 'Directives', icon: 'list' },
-  { name: 'DataBinding', path: '/DataBinding', component: DataBinding, display: 'DataBinding', icon: 'fas fa-expand' },
-  { name: 'Models', path: '/Models', component: Models, display: 'Models', icon: 'clone' },
+  { name: 'DataBinding', path: '/DataBinding', component: DataBinding, display: 'DataBinding', icon: 'info' },
+  { name: 'Models', path: '/Models', component: Models, display: 'Models', icon: 'info' },
   {
     name: 'Routes',
     path: '/Routes',
     component: Routes,
     display: 'Routes',
-    icon: 'router',
+    icon: 'info',
     children: [
       { path: 'Child/:id', component: Child }
     ]
   },
-  { name: 'ListRendering', path: '/ListRendering', component: ListRendering, display: 'ListRendering', icon: 'list' }
+  { name: 'ListRendering', path: '/ListRendering', component: ListRendering, display: 'ListRendering', icon: 'list' },
+  { name: 'Computed', path: '/Computed', component: Computed, display: 'Computed', icon: 'graduation-cap' }
 ]
