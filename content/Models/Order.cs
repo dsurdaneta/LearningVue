72ce9b39e6d4edeb3324e34707124d6eaf0a014e
Daniel Urdaneta <dsurdaneta@gmail.com>
2019-11-19 14:01:28 +0100

    added demo models

---
+++ content/Models/Order.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
+
+namespace Vue2Spa.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+        public int CustomerId { get; set; }
+        //[Computed]
+        public decimal Total { get; set; }
+        //[Write(false)]
+        //public Customer Customer { get; set; }
+        //[Write(false)]
+        //public List<OrderItems> Items { get; set; }
+    }
+}
